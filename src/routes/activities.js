840f671f358e69bb0ec25f5857f33c4d5ec3557b
Pieter Bommele <pieter.bommele@student.hogent.be>
2021-10-20 08:08:19 +0200

    updated routes

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -15,17 +15,29 @@ router.get(
         ctx.body = activities
     }
 );
-
+router.get('/api/activities/:id',
+async ctx => {
+    if (!ctx.state.userAuthenticated){
+        ctx.throw("403", "User authentication required");
+    }
+    const favorites = await prisma.activity.findMany({
+        where: {
+            createdby: parseInt(ctx.request.params.id)
+        }
+    })
+    ctx.body = favorites
+}
+)
 router.post("/api/activities",
     {
         validate: {
             type: "json",
             body: {
-                activity: Joi.string()
-                    .required(),
+                activity: Joi.string().required(),
                 type: Joi.string().required(),
                 participants: Joi.number().required(),
-                accessibility: Joi.number().required()
+                accessibility: Joi.number().required(),
+                createdby: Joi.number()
             }
         }   
     },
@@ -37,12 +49,17 @@ router.post("/api/activities",
             const type = ctx.request.body.type;
             const participants = ctx.request.body.participants;
             const accessibility = ctx.request.body.accessibility;
+            const createdby = null;
+            if (ctx.request.body.createdby){
+                createdby = ctx.request.body.createdby;
+            }
             const result = await prisma.activity.create({
                 data: {
                   activity,
                   type,
                   participants,
-                  accessibility
+                  accessibility,
+                  createdby
                 },
               });
             const activities = await prisma.activity.findMany()
