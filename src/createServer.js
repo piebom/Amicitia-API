0705d7edf28a8a08fd9832ec88e3f66b8c1751d4
Pieter Bommele <pieter.bommele@student.hogent.be>
2021-12-24 01:46:13 +0100

    update

--- src/createServer.js
+++ src/createServer.js
@@ -21,7 +21,7 @@ module.exports = async function createServer (){
   const app = new Koa();
   const logger = getLogger();
 	app.use(
-		koaCors({ origin : [ "https://activityjournal-api.herokuapp.com", "https://frontendweb-pieter-2122-pie-bomm-j4g8ernrl-piebomm.vercel.app/"]})
+		koaCors({ origin : [ "https://frontendweb-pieter-2122-pie-bomm-j4g8ernrl-piebomm.vercel.app/"]})
 	);
   app.use(async (ctx,next) => {
     const logger = getLogger();
