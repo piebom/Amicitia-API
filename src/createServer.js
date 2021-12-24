2ba4ff288ab933467049cd1486c12b58cba3eba5
Pieter Bommele <pieter.bommele@student.hogent.be>
2021-12-24 04:40:16 +0100

    migrations and seeds + tests

--- src/createServer.js
+++ src/createServer.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from ".prisma/client";
+const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 const Router = require('@koa/router');
 const Koa = require('koa');
